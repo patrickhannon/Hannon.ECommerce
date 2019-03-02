de581cba93e90a76f5880cfab907ff0616e97a7c
Patrick Hannon <patrick_hannon@hotmail.com>
2019-03-01 19:02:27 -0600

    couple tweaks

---
+++ git.bat
@@ -0,0 +1,8 @@
+git remote add origin https://github.com/patrickhannon/Hannon.LDAP.git
+git push -u origin master
+REM echo "# Hannon.ECommerce" >> README.md
+REM git init
+REM git add README.md
+REM git commit -m "first commit"
+REM git remote add origin git@github.com:patrickhannon/Hannon.ECommerce.git
+REM git push -u origin master
